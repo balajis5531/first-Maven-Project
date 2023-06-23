1cafb5d01d996c8f654ab0fbb52c4ed090b749c8
Balajis007 <donbalajis007@gmail.com>
2023-06-23 10:21:08 +0530

    day17

---
+++ src/main/java/com/fssa/balaji/day17/solved/HelloWordProg.java
@@ -0,0 +1,12 @@
+package com.fssa.balaji.day17.solved;
+
+public class HelloWordProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+
+	}
+
+}
